ef8d1da41218ce40ca89df535daa49c28a46d610
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-11-12 13:16:55 +0100

    [FIX]Stock move line:Update the Quality of line according to the Quality of the related product

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -7,10 +7,6 @@ from odoo.exceptions import UserError
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    product_quality_id = fields.Many2one('product.quality', string='Quality',compute='_compute_product_quality_id',store=True)
+    product_quality_id = fields.Many2one('product.quality', string='Quality',related='product_id.product_quality_id',store=True)
 
-    @api.depends('product_id')
-    def _compute_product_quality_id(self):
-        for each in self:
-            each.product_quality_id = each.product_id.product_quality_id and each.product_id.product_quality_id.id or False
 
